2b962a6d6ff93811e6c9698648b20382f2479e5f
zhurainyk <zhurainyk@163.com>
2022-08-25 15:28:53 +0800

    add

---
+++ static/js/chunk-2d0d3e58.2abe2e00.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([["chunk-2d0d3e58"],{"5f5f":function(n,e,u){"use strict";u.r(e);var t=u("2877"),c=Object(t.a)({},(function(){return(0,this._self._c)("router-view")}),[],!1,null,null,null);e.default=c.exports}}]);
